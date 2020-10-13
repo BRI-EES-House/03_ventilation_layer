3003792939f634f201a75ab3a3a39670b70f0a2f
TSUJIMARU-PC\Norie Tsujimaru <tsujimaru@satoh-er.com>
2020-10-13 14:54:01 +0900

    envelope_performance_factorsにおいて、日射熱取得率の計算を修正

--- envelope_performance_factors.py
+++ envelope_performance_factors.py
@@ -28,12 +28,19 @@ def solar_heat_gain_coefficient(parm: ventilation_wall.Parameters, theta_as_ave:
 
     # 固定値を設定
     # Note:固定値はグローバル変数にするのがよいか
-    h_in = 9.0  # 室内側総合熱伝達率, W/(m2・K)
+    h_out = 25.0    # 室外側総合熱伝達率, W/(m2・K)
+    r_s_e = 0.11  # 室外側表面熱伝達抵抗, (m2・K)/W, 省エネ基準の規定値
+    r_s_r = 0.11  # 室内側表面熱伝達抵抗, (m2・K)/W, 省エネ基準の規定値
+    # Note: 省エネ基準の規定による表面熱伝達抵抗は、屋根、外壁で値が異なる。どのように判定するかが課題。
 
+    # 相当外気温度を計算
+    theta_SAT = get_theta_SAT(parm.theta_e, parm.a_surf, parm.J_surf, h_out)
 
+    # 温度、風速依存の熱伝達率を使用したU値を計算
+    u_s_dash = get_u_s_dash(r_s_e, r_s_r, parm.C_2, h_cv, h_rv)
 
     # 通気層を有する壁体の日射熱取得率(-)を計算
-    eta_e = h_in * (matrix_temp[3][0] - parm.theta_r) / parm.J_surf
+    eta_e = u_s_dash * parm.a_surf/h_out * (theta_as_ave - parm.theta_r) / (theta_SAT - parm.theta_r)
 
     return eta_e
 
