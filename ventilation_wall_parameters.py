ecdb63673688393206d4a3ce10e2a2c5032cc07a
TSUJIMARU-PC\Norie Tsujimaru <tsujimaru@satoh-er.com>
2020-12-21 14:41:23 +0900

    関数get_parameter_listを修正（各パラメータに与える水準値を変更）

--- ventilation_wall_parameters.py
+++ ventilation_wall_parameters.py
@@ -1,7 +1,9 @@
 import itertools
 import pandas as pd
+import numpy as np
 import global_number
 import ventilation_wall as vw
+import envelope_performance_factors as epf
 
 
 class Log:
@@ -17,25 +19,25 @@ class Log:
 def get_parameter_list() -> object:
     """
     複数のパラメータの総当たりの組み合わせ（直積）のリストを作成する
-    :arg:
+    :param なし
     :return: 総当たりのパラメータリスト
     """
 
-    # TODO: テスト用にパラメータ数を制限しているため、最終的には想定したパラメータに合わせて変更すること
-    theta_e = [round(x * 1.0, 1) for x in range(-20, 50, 10)]    # 外気温度, degree C
-    theta_r = [25.0]                                            # 室内温度,　degree C
-    j_surf = [round(x * 1.0, 1) for x in range(0, 1100, 200)]    # 外気側表面に入射する日射量, W/m2
-    a_surf = [round(x * 0.1, 1) for x in range(0, 11, 2)]       # 外気側表面日射吸収率
-    C_1 = [round(x * 0.1, 1) for x in range(5, 20, 5)]          # 外気側部材の熱コンダクタンス,W/(m2・K)
-    C_2 = [round(x * 0.1, 1) for x in range(5, 20, 5)]          # 室内側部材の熱コンダクタンス, W/(m2・K)
-    l_h = [3.0]                 # 通気層の長さ, m
-    l_w = [3.0]                 # 通気層の幅, m
-    l_d = [0.3]                 # 通気層の厚さ, m
-    angle = [0.0, 45.0, 90.0]   # 通気層の傾斜角, °
-    v_a = [1.0]                 # 通気層の平均風速, m/s
-    l_s = [0.45]                # 通気胴縁または垂木の間隔, m
-    emissivity_1 = [1.0]        # 通気層に面する面1の放射率, -
-    emissivity_2 = [1.0]        # 通気層に面する面2の放射率, -
+    # Note: 各パラメータには、一部を除いて想定される上下限値と中央値の3点を与える
+    theta_e = np.array([-20.0, np.median([-20.0, 40.0]), 40.0], dtype=float)        # 外気温度, degree C
+    theta_r = np.array([20.0, np.median([20.0, 27.0]), 27.0], dtype=float)          # 室内温度,　degree C
+    j_surf = np.array([0.0, np.median([0.0, 1000.0]), 1000.0], dtype=float)         # 外気側表面に入射する日射量, W/m2
+    a_surf = np.array([0.0, np.median([0.0, 1.0]), 1.0], dtype=float)               # 外気側表面日射吸収率
+    C_1 = np.array([0.5, np.median([0.5, 100.0]), 100.0], dtype=float)              # 外気側部材の熱コンダクタンス,W/(m2・K)
+    C_2 = np.array([0.1, np.median([0.1, 5.0]), 5.0], dtype=float)                  # 室内側部材の熱コンダクタンス, W/(m2・K)
+    l_h = np.array([3.0, np.median([3.0, 12.0]), 12.0], dtype=float)                # 通気層の長さ, m
+    l_w = np.array([0.05, np.median([0.05, 10.0]), 10.0], dtype=float)              # 通気層の幅, m
+    l_d = np.array([0.05, np.median([0.05, 0.3]), 0.3], dtype=float)                # 通気層の厚さ, m
+    angle = np.array([0.0, np.median([0.0, 90.0]), 90.0], dtype=float)              # 通気層の傾斜角, °
+    v_a = np.array([0.0, np.median([0.0, 1.0]), 1.0], dtype=float)                  # 通気層の平均風速, m/s
+    l_s = [0.45]                                                                    # 通気胴縁または垂木の間隔, m
+    emissivity_1 = [0.9]                                                            # 通気層に面する面1の放射率, -
+    emissivity_2 = np.array([0.1, np.median([0.1, 0.9]), 0.9], dtype=float)         # 通気層に面する面2の放射率, -
 
     parameter_list = list(itertools.product(theta_e, theta_r, j_surf, a_surf, C_1, C_2, l_h, l_w, l_d, angle,
                                             v_a, l_s, emissivity_1, emissivity_2))
