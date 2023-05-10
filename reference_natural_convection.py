8f3aae34c60696a85a576a1cccdabf534639fbb5
Hisashi Miura <miura_hisashi@hotmail.com>
2023-05-10 12:16:29 +0900

    update the comments of the functions

--- reference_natural_convection.py
+++ reference_natural_convection.py
@@ -1,15 +1,21 @@
 import math
-from ventilation_layer.global_number import get_gr_air, get_pr_air, get_lambda_air
+
+from ventilation_layer import global_number as gn
 
 
 def calc_takeda_method(delta_t: float, direction: str) -> float:
+    '''Calculate the natural convective heat transfer coefficient by Takeda method.
 
-    '''
-    武田仁他：最新建築環境工学　第４版
-    自然対流熱伝達率の計算
-    :param delta_t:温度差[C]
-    :param direction:向き　'v':垂直、'u':上向き、'd':下向き
-    :return:熱伝達率, W/(m2 K)
+    Args:
+        delta_t: 温度差, degrees
+        direction: 向き
+            'v':垂直
+            'u':上向き
+            'd':下向き
+    Returns:
+        熱伝達率, W/(m2 K)
+    Notes:
+        武田仁他：最新建築環境工学 第４版
     '''
 
     if direction == 'v':        # 垂直
@@ -18,27 +24,34 @@ def calc_takeda_method(delta_t: float, direction: str) -> float:
         return 2.67 * delta_t ** 0.25
     elif direction == 'd':      # 水平上向き
         return (0.64 + 0.87) / 2.0 * delta_t ** 0.25
+    else:
+        raise Exception()
 
 
 def calc_shase_handbook_method(tw: float, tf: float, d: float, direction: str) -> float:
+    '''自然対流熱伝達率の計算
 
-    '''
-    空気調和・衛生工学便覧　第14班
-    自然対流熱伝達率の計算
-    :param tw: 表面温度[C]
-    :param tf: 流体温度[C]
-    :param d: 代表長さ[m]
-    :param direction: 向き　'v':垂直、'u':上向き、'd':下向き
-    :return: 熱伝達率, W/(m2 K)
+    Args:
+        tw: 表面温度, degrees
+        tf: 流体温度, degrees
+        d: 代表長さ, m
+        direction: 向き
+            'v':垂直
+            'u':上向き
+            'd':下向き
+    Returns:
+        熱伝達率, W/(m2 K)
+    Notes:
+        空気調和・衛生工学便覧 第14班
     '''
 
     # 膜温度
     t_ave = (tw + tf) / 2.0
 
     # プラントル数の計算
-    pr = get_pr_air(t=t_ave)
+    pr = gn.get_pr_air(t=t_ave)
     # グラスホフ数の計算
-    gr = get_gr_air(tw=tw, tf=tf, d=d)
+    gr = gn.get_gr_air(tw=tw, tf=tf, d=d)
 
     # ヌセルト数の初期化
     nu = 0.0
@@ -49,37 +62,51 @@ def calc_shase_handbook_method(tw: float, tf: float, d: float, direction: str) -
             nu = 4.0 / 3.0 * f_pr * (gr * pr) ** 0.25
         elif gr * pr >= 1.0e9:
             nu = 0.13 * gr ** (1.0 / 3.0)
+        else:
+            raise Exception()
     elif direction == 'u':      # 水平上向き
         if 3.0e5 < gr * pr < 3.0e10:
             nu = 0.27 * (gr * pr) ** 0.25
+        else:
+            raise Exception()
     elif direction == 'd':      # 水平下向き
         if 1.0e5 < gr * pr < 2.0e7:
             nu = 0.54 * (gr * pr) ** 0.25
         elif 2.0e7 < gr * pr < 3.0e10:
             nu = 0.14 * (gr * pr) ** (1.0 / 3.0)
+        else:
+            raise Exception()
+    else:
+        raise Exception()
 
-    return get_lambda_air(t=t_ave) * nu / d
+    return gn.get_lambda_air(t=t_ave) * nu / d
 
 
 def calc_kimura_method(tw: float, tf: float, d: float, direction: str) -> float:
 
-    '''
-    木村建一　建築設備基礎
-    自然対流熱伝達率の計算
-    :param tw: 表面温度[C]
-    :param tf: 流体温度[C]
-    :param d: 代表長さ[m]
-    :param direction: 向き　'v':垂直、'u':上向き、'd':下向き
-    :return: 熱伝達率, W/(m2 K)
+    '''自然対流熱伝達率の計算
+
+    Args:
+        tw: 表面温度, degrees
+        tf: 流体温度, degrees
+        d: 代表長さ, m
+        direction: 向き
+            'v':垂直
+            'u':上向き
+            'd':下向き
+    Returns:
+        熱伝達率, W/(m2 K)
+    Notes:
+        木村建一: 建築設備基礎
     '''
 
     # 膜温度
     t_ave = (tw + tf) / 2.0
 
     # プラントル数の計算
-    pr = get_pr_air(t=t_ave)
+    pr = gn.get_pr_air(t=t_ave)
     # グラスホフ数の計算
-    gr = get_gr_air(tw=tw, tf=tf, d=d)
+    gr = gn.get_gr_air(tw=tw, tf=tf, d=d)
 
     if direction == 'v':        # 垂直
         if gr * pr < 1.0e9:
@@ -97,23 +124,29 @@ def calc_kimura_method(tw: float, tf: float, d: float, direction: str) -> float:
 
 def calc_ashrae_handbook_method(tw: float, tf: float, L: float, direction: str) -> float:
 
-    '''
-    ASHRAE Handbook Fundamentals 2013
-    自然対流熱伝達率の計算
-    :param tw: 表面温度[C]
-    :param tf: 竜体温度[C]
-    :param L: 代表長さ[m]（垂直：高さ、水平：A/L_peri）
-    :param direction: 向き　'v':垂直、'u':上向き、'd':下向き
-    :return: 熱伝達率, W/(m2 K)
+    '''自然対流熱伝達率の計算
+
+    Args:
+        tw: 表面温度, degrees
+        tf: 流体温度, degrees
+        L: 代表長さ, m (垂直：高さ、水平: A/L_peri)
+        direction: 向き
+            'v':垂直
+            'u':上向き
+            'd':下向き
+    Returns:
+        熱伝達率, W/(m2 K)
+    Notes:
+        ASHRAE Handbook Fundamentals 2013
     '''
 
     # 膜温度
     t_ave = (tw + tf) / 2.0
 
     # プラントル数の計算
-    pr = get_pr_air(t=t_ave)
+    pr = gn.get_pr_air(t=t_ave)
     # グラスホフ数の計算
-    gr = get_gr_air(tw=tw, tf=tf, d=L)
+    gr = gn.get_gr_air(tw=tw, tf=tf, d=L)
     # レーリー数の計算
     ra = pr * gr
 
@@ -123,6 +156,8 @@ def calc_ashrae_handbook_method(tw: float, tf: float, L: float, direction: str) 
             nu = 0.68 + 0.67 * ra ** 0.25 / (1.0 + (0.492 / pr) ** (9.0 / 16.0)) ** (4.0 / 9.0)
         elif 1.0e9 < ra < 1.0e12:
             nu = (0.825 + 0.387 * ra ** (1.0 / 6.0) / (1.0 + (0.492 / pr) ** (9.0 / 16.0)) ** (8.0 / 27.0)) ** 2.0
+        else:
+            raise Exception()
     elif direction == 'u':      # 水平上向き
         if 1.0 < ra < 200.0:
             nu = 0.96 * ra ** (1.0 / 6.0)
@@ -132,47 +167,48 @@ def calc_ashrae_handbook_method(tw: float, tf: float, L: float, direction: str) 
             nu = 0.54 * ra ** 0.25
         elif 8.0e6 < ra < 1.5e9:
             nu = 0.15 * ra ** (1.0 / 3.0)
+        else:
+            raise Exception()
     elif direction == 'd':      # 水平下向き
         if 1.0e5 < ra < 1.0e10:
             nu = 0.27 * ra ** 0.25
+        else:
+            raise Exception()
+    else:
+        raise Exception()
 
-    return nu * get_lambda_air(t=t_ave) / L
+    return nu * gn.get_lambda_air(t=t_ave) / L
 
 
 def calc_jsme_databook(tw: float, tf: float, L: float, b: float) -> float:
 
-    '''
-    日本機械学会　伝熱工学資料改訂第5版
-    鉛直平行平板間の自然対流熱伝達率の計算
-    :param tw: 表面温度[C]
-    :param tf: 竜体温度[C]
-    :param L: 代表長さ[m]（垂直：高さ）
-    :param b: 平行平板の間隔[m]
-    :return: 熱伝達率, W/(m2 K)
+    '''鉛直平行平板間の自然対流熱伝達率の計算
+
+    Args:
+        tw: 表面温度, degrees
+        tf: 流体温度, degrees
+        L: 代表長さ, m （垂直：高さ）
+        b: 平行平板の間隔, m
+    Returns:
+        熱伝達率, W/(m2 K)
+    Notes:
+        日本機械学会　伝熱工学資料改訂第5版
     '''
 
     # 膜温度
     t_ave = (tw + tf) / 2.0
 
     # プラントル数の計算
-    pr = get_pr_air(t=t_ave)
-    # print('Pr=', pr)
+    pr = gn.get_pr_air(t=t_ave)
+
     # グラスホフ数の計算
-    gr = get_gr_air(tw=tw, tf=tf, d=b)
-    # print('Gr=', gr)
+    gr = gn.get_gr_air(tw=tw, tf=tf, d=b)
+
     # 修正レーリー数の計算
     ra = pr * gr * b / L
-    # print('Rab=', ra)
-    # print('log(ra)=', math.log(ra))
 
     nu = (1.0 / 24.0) * ra \
          / (1.0 + (1.0 / 18.0) ** 1.5 * (1.0 + (0.492 / pr) ** (9.0 / 16.0)) ** (2.0 / 3.0) * ra ** (9.0 / 8.0)) ** (2.0 / 3.0)
-    # print('nu1=', nu)
-    # nu = 1.0 / 18.8 * ra ** 0.85 * (1.0 - math.exp(-52.0 / ra)) ** 0.6
-    # print('nu2=', nu)
 
-    return nu * get_lambda_air(t=t_ave) / L
+    return nu * gn.get_lambda_air(t=t_ave) / L
 
-
-if __name__ == '__main__':
-    print(calc_jsme_databook(tw=5, tf=18, L=3.0, b=0.03))
